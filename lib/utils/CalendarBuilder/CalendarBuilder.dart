4e7dbddc574fc38cdbc312e7b48f860fc1c28022
tranzal <64002448+tranzal@users.noreply.github.com>
2022-05-26 20:29:05 +0900

    차트 추가

--- lib/utils/CalendarBuilder/CalendarBuilder.dart
+++ lib/utils/CalendarBuilder/CalendarBuilder.dart
@@ -1,5 +1,4 @@
 import 'package:accountbook/hive/CalendarTypeAdapter.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
---
+++ lib/widgets/ChartCustomWidget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class ChartCustomWidget extends StatelessWidget {
+  const ChartCustomWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: CustomPaint(
+
+      ),
+    );
+  }
+}
